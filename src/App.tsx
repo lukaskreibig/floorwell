82291a773c7ff7f73e64ff5e718f3e4b2f3cca93
Lukas Kreibig <78150333+lukaskreibig@users.noreply.github.com>
2022-05-13 11:31:13 +0200

    add API fetch material/materialClass

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [material, setMaterial] = useState<any>(null);
+  const [materialClass, setMaterialClass] = useState<any>(null);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<any>("");
+
+  useEffect((): void => {
+    const getData = async (): Promise<void> => {
+      try {
+        setLoading(true);
+        const [materialFetch, classFetch] = await Promise.all(
+          [
+            fetch(
+              `/api/fw_material`, 
+          {
+          headers: {
+            'X-ApiKey': 'ZGF0IGdpYnQgZWluIGJvbnVzcHVua3Q='
+          }
+        }
+            ), fetch(`/api/fw_materialclass`, 
+            {
+            headers: {
+              'X-ApiKey': 'ZGF0IGdpYnQgZWluIGJvbnVzcHVua3Q='
+            }
+          })
+          ]
+        );
+        if (!materialFetch.ok || !classFetch.ok) {
+          setLoading(false);
+          throw new Error(
+            `Oh No! A failure occured fetching ${
+              !materialFetch.ok
+                ? `Material Data ${materialFetch.status}`
+                : `Class Material: ${classFetch.status}`
+            }`
+          );
+        }
+        let materialData = await materialFetch.json();
+        let classData = await classFetch.json();
+        setMaterial(materialData);
+        setMaterialClass(classData);
+        setError(null);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  console.log("Material", material)
+  console.log("MaterialClass", materialClass)
+
+
+  return (null)
+
+
 }
 
 export default App;
